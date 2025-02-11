20e52d4a631858f4b95eef6c4c9aeb6e995d1d79
ShrikeSwallow <maciej.jaskolka@gmail.com>
2025-02-11 06:40:01 +0000

    Implement 'routing' using switch

--- index.js
+++ index.js
@@ -7,8 +7,29 @@ const server = createServer();
 server.on("request", (req, res) => {
   const myUrl = parse(req.url);
   const filename = `.${myUrl.pathname}`;
-  console.log(filename);
-  readFile(filename, (err, data) => {
+  let page = "./";
+  switch (filename) {
+    case "./": {
+      page = "./index.html";
+      break;
+    }
+    case "./index.html": {
+      page = "./index.html";
+      break;
+    }
+    case "./about.html": {
+      page = "./about.html";
+      break;
+    }
+    case "./contact-me.html": {
+      page = "./contact-me.html";
+      break;
+    }
+    default:
+      page = "./404.html";
+  }
+  console.log(page);
+  readFile(page, (err, data) => {
     if (err) {
       res.writeHead(404, { "content-type": "text/html" });
       return res.end("404 Not Found");
